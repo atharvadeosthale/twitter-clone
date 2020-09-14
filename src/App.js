ed22bc8d8022c3f79d0dfab5c1b5df1fe7f0f304
Atharva Deosthale <atharva.deosthale17@gmail.com>
2020-09-14 20:29:00 +0530

    Sidebar feed initialized

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <TwitterIcon />
+      <SidebarOption Icon={HomeIcon} text="Home" active={true} />
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
+    </div>
+  );
+}
+
+export default Sidebar;
